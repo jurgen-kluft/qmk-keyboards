5b7d95adbcc0739f484c3b8b1ff4d477cd22b011
Jurgen <jurgen.kluft@gmail.com>
2022-04-11 14:42:37 +0800

    * oryx = off

--- moonlander/keymaps/jaykay/rules.mk
+++ moonlander/keymaps/jaykay/rules.mk
@@ -1,9 +1,10 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+AUDIO_ENABLE = no          # Audio output
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 WEBUSB_ENABLE = yes
-ORYX_ENABLE = yes
+ORYX_ENABLE = no
 TAP_DANCE_ENABLE = no
 
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
