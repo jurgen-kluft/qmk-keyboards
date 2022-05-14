ae53106495a2ee0b60f4878367c448256970258c
Jurgen <jurgen.kluft@gmail.com>
2022-05-14 10:31:13 +0800

    * oneshot; 8 bit keycodes

--- shared/jaykay/user_oneshot.c
+++ shared/jaykay/user_oneshot.c
@@ -1,40 +1,41 @@
 #include QMK_KEYBOARD_H
+#include "config.h"
 #include "feature.h"
 #include "oneshot.h"
-#include "user_oneshot.h"
+#include "user_keycodes.h"
+#include "layers.h"
 
 #ifdef ENABLE_ONESHOT
 
-bool is_oneshot_modifier_cancel_key(uint16_t keycode, bool pressed)
+bool is_oneshot_modifier_cancel_key(uint8_t keycode, bool pressed)
 {
     switch (keycode)
     {
-        case KC_FNAV:
-        case KC_FCAPS: return pressed;
+        case CC_FNAV:
+        case CC_FCAPS: return pressed;
     }
     return false;
 }
 
-bool is_oneshot_modifier_ignored_key(uint16_t keycode, bool pressed)
+bool is_oneshot_modifier_ignored_key(uint8_t keycode, bool pressed)
 {
-    switch (keycode)
-    {
-        case KC_FNAV:
-        case KC_FSYM: return true;
-    }
+    if (keycode >= CC_RANGE_START && keycode <= CC_RANGE_END)
+        return true;
     return false;
 }
 
-oneshot_mod get_modifier_for_trigger_key(uint16_t keycode)
+oneshot_mod get_modifier_for_trigger_key(uint8_t keycode)
 {
     switch (keycode)
     {
-        case OS_CTRL:
-        case OS_SHFT:
-        case OS_ALT:
-        case OS_CMD: return (keycode - OS_CTRL) + ONESHOT_LCTL;
+        case CC_CTRL:
+        case CC_SHFT:
+        case CC_ALT:
+        case CC_CMD: return (keycode - CC_CTRL) + ONESHOT_LCTL;
     }
     return ONESHOT_NONE;
 }
 
+
 #endif
+
