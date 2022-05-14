8ae75683523135e17daea12d11b8ced81f7123fd
Jurgen <jurgen.kluft@gmail.com>
2022-05-14 10:44:11 +0800

    * rules; adding vim, user layers, user keycodes source files

--- shared/jaykay/rules.mk
+++ shared/jaykay/rules.mk
@@ -3,6 +3,7 @@ SRC += $(USER_PATH)/cushi.c
 SRC += $(USER_PATH)/cukey.c
 SRC += $(USER_PATH)/feature.c
 SRC += $(USER_PATH)/leader.c
+SRC += $(USER_PATH)/vim.c
 
 # All the user implementations are below
 ifeq ($(strip $(ENCODER_ENABLE)), yes)
@@ -12,3 +13,7 @@ SRC += $(USER_PATH)/user_keyboard_init.c
 SRC += $(USER_PATH)/user_oneshot.c
 SRC += $(USER_PATH)/user_process_record.c
 SRC += $(USER_PATH)/user_leader.c
+SRC += $(USER_PATH)/user_keycodes.c
+SRC += $(USER_PATH)/user_layers.c
+
+
