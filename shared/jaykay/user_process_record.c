a993a22fd17fbb8db060a63473054338004d0ee9
Jurgen <jurgen.kluft@gmail.com>
2022-05-14 10:43:39 +0800

    * process record user; 8 bit keycodes and custom register/unregister of qmk keycodes using current layer

--- shared/jaykay/user_process_record.c
+++ shared/jaykay/user_process_record.c
@@ -1,12 +1,15 @@
 #include QMK_KEYBOARD_H
+#include "config.h"
 #include "layers.h"
 #include "oneshot.h"
 #include "cushi.h"
 #include "cukey.h"
 #include "feature.h"
+#include "leader.h"
+#include "vim.h"
 #include "user_leader.h"
-#include "user_secrets.h"
 #include "user_keycodes.h"
+#include "user_layers.h"
 
 #ifdef OLED_ENABLE
 #include "oled.h"
@@ -19,15 +22,17 @@ static const char* gSecrets[] = {SECRET_1, SECRET_2, SECRET_3, SECRET_4, SECRET_
 static const char* gSecrets[] = {"SECRET_1", "SECRET_2", "SECRET_3", "SECRET_4", "SECRET_5", "SECRET_6", "SECRET_7", "SECRET_8"};
 #endif
 
-bool process_record_user(uint16_t keycode, keyrecord_t* record)
+bool process_record_user(uint16_t _keycode, keyrecord_t* record)
 {
+    uint16_t keycode = user_layer_get_code(_keycode, record->event.pressed);
+
 #ifdef OLED_DRIVER_ENABLE
-    process_record_oled(keycode, record);
+    process_record_oled(_keycode, record);
 #endif
 
     switch (keycode)
     {
-        case KC_OS_UNDO ... KC_OS_CLOSE:
+        case CC_UNDO ... CC_CLOSE:
         {
             if (record->event.pressed)
             {
@@ -44,55 +49,78 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record)
 
             return false;
         }
-        case KC_SECRET_1 ... KC_SECRET_8: // Secrets!  Externally defined strings, not stored in repo
+        case CC_SECRET_1 ... CC_SECRET_8:
             if (!record->event.pressed)
             {
                 turnoff_oneshot_modifiers();
-                send_string_with_delay(gSecrets[keycode - KC_SECRET_1], MACRO_TIMER);
+                send_string_with_delay(gSecrets[keycode - CC_SECRET_1], MACRO_TIMER);
             }
             return false;
             break;
-        case KC_OLED:
+        case CC_SPIFT:
             if (record->event.pressed)
             {
-#ifdef OLED_ENABLE
+                register_code(KC_SPACE);
+                press_oneshot_modifier(ONESHOT_LSFT);
+            }
+            else
+            {
+                unregister_code(KC_SPACE);
+                release_oneshot_modifier(ONESHOT_LSFT);
+            }
+            break;
+#if defined(OLED_ENABLE)
+        case CC_OLED:
+            if (record->event.pressed)
+            {
                 toggle_display_oled();
-#endif
-#ifdef RGBLIGHT_ENABLE
-                rgblight_enable();
-#endif
             }
             return true;
+#elif defined(RGBLIGHT_ENABLE)
+        case CC_OLED:
+            if (record->event.pressed)
+            {
+                rgblight_enable();
+            }
+            return true;
+#endif
     }
 
-    uint16_t new_keycode = process_cushi_keys(keycode, record, true);
-    if (!process_feature_key(new_keycode, record))
+    if (vim_is_active())
     {
+        process_vim(keycode, record);
         return false;
     }
 
-    keycode = process_cushi_keys(keycode, record, false);
-
-    update_oneshot_modifiers(keycode, record);
-
-    switch (keycode)
+    if (!leader_is_active() && !vim_is_active())
     {
-        case KC_QWERTY:
-            if (record->event.pressed)
-            {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            break;
-        case KC_RSTHD:
-            if (record->event.pressed)
-            {
-                set_single_persistent_default_layer(_RSTHD);
-            }
-            break;
+        if (!process_feature_key(keycode, record))
+        {
+            return false;
+        }
+
+        update_oneshot_modifiers(keycode, record);
+
+        switch (keycode)
+        {
+            case CC_QWERTY:
+                if (record->event.pressed)
+                {
+                    set_single_persistent_default_layer(LAYER_QWERTY);
+                }
+                break;
+            case CC_RSTHD:
+                if (record->event.pressed)
+                {
+                    set_single_persistent_default_layer(LAYER_RSTHD);
+                }
+                break;
+        }
     }
 
     if (process_leader_user(keycode, record))
         return false;
 
-    return true;
+    user_apply_keycode(keycode, record->event.pressed);
+    return false;
 }
