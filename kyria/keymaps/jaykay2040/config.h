35028b2a27b59121167b855c8ae29fd9f20d4353
Jurgen <jurgen.kluft@gmail.com>
2022-06-13 19:45:22 +0800

    * kyria rp2040; works

--- kyria/keymaps/jaykay2040/config.h
+++ kyria/keymaps/jaykay2040/config.h
@@ -12,22 +12,68 @@
 #undef RGB_DI_PIN
 #undef SPLIT_HAND_MATRIX_GRID
 
-#define MATRIX_ROW_PINS {GP27, GP26, GP22, GP20}
-#define MATRIX_ROW_PINS_RIGHT {GP4, GP5, GP6, GP7}
-#define MATRIX_COL_PINS {GP23, GP21, GP9, GP8, GP7, GP6, GP5, GP4}
-#define MATRIX_COL_PINS_RIGHT {GP8, GP9, GP21, GP23, GP20, GP22, GP26, GP27}
+// F6 = GP27
+// F7 = GP26
+// B1 = GP22
+// B3 = GP20
+// 
+// B2 = GP23
+// B6 = GP21
+// B5 = GP9
+// B4 = GP8
+// E6 = GP7
+// D7 = GP6
+// C6 = GP5
+// D4 = GP4
+// 
+// F4 = GP29
+// F5 = GP28
 
-#define ENCODERS_PAD_A {GP29}
-#define ENCODERS_PAD_B {GP28}
-#define ENCODERS_PAD_A_RIGHT {GP28}
-#define ENCODERS_PAD_B_RIGHT {GP29}
+// clang-format off
+#ifdef KYRIA_REV2
 
-#define SERIAL_USART_TX_PIN GP1
-#define RGB_DI_PIN GP0
-#define SPLIT_HAND_MATRIX_GRID GP7, GP20
-#define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP16
-#define I2C1_SCL_PIN GP17
+    #define MATRIX_ROW_PINS {GP27, GP26, GP22, GP20}
+    #define MATRIX_ROW_PINS_RIGHT {GP4, GP5, GP6, GP7}
+    #define MATRIX_COL_PINS {GP23, GP21, GP9, GP8, GP7, GP6, GP5, GP4}
+    #define MATRIX_COL_PINS_RIGHT {GP8, GP9, GP21, GP23, GP20, GP22, GP26, GP27}
+
+    #define ENCODERS_PAD_A {GP29}
+    #define ENCODERS_PAD_B {GP28}
+    #define ENCODERS_PAD_A_RIGHT {GP28}
+    #define ENCODERS_PAD_B_RIGHT {GP29}
+
+    #define SERIAL_USART_TX_PIN GP1
+    #define RGB_DI_PIN GP0
+    #define SPLIT_HAND_MATRIX_GRID GP7, GP20
+    #define I2C_DRIVER I2CD1
+    #define I2C1_SDA_PIN GP16
+    #define I2C1_SCL_PIN GP17
+
+#else // rev1
+
+    #define MATRIX_ROW_PINS { GP8, GP7, GP6, GP4 }
+    #define MATRIX_COL_PINS { GP21, GP23, GP20, GP22, GP26, GP27, GP28, GP29 }
+    #define UNUSED_PINS
+
+    #define ENCODERS_PAD_A { GP5 }
+    #define ENCODERS_PAD_B { GP9 }
+    #define ENCODERS_PAD_A_RIGHT { GP9 }
+    #define ENCODERS_PAD_B_RIGHT { GP5 }
+
+    #define I2C_DRIVER I2CD2
+    #define I2C1_SDA_PIN GP2
+    #define I2C1_SCL_PIN GP3
+
+    //#define SERIAL_USART_DRIVER SIOD0
+    //#define SERIAL_USART_TX_PIN GP0
+    //#define SERIAL_USART_RX_PIN GP1
+
+    #define RGB_DI_PIN GP0
+    #define SOFT_SERIAL_PIN GP1
+
+#endif
+// clang-format on
+
 // #define SPLIT_POINTING_ENABLE
 // #define POINTING_DEVICE_RIGHT
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
@@ -76,4 +122,4 @@
 // then define EE_HANDS and flash your sides with 'avrdude-split-left' and 'avrdude-split-right'.
 // This makes sure that the firmware on the left sides identifies itself as 'left' and the right side
 // identifies itself as 'right'.
-#define EE_HANDS
+//#define EE_HANDS
--- kyria/keymaps/jaykay2040/notes.md
+++ kyria/keymaps/jaykay2040/notes.md
@@ -3,4 +3,5 @@
 ## QMK RP2040 support
 
 https://github.com/KarlK90/qmk_firmware/tree/feature/raspberry-pi-rp2040-support
+https://github.com/waffle87/qmk_firmware/tree/waffle_rp2040/keyboards/splitkb/kyria/keymaps/waffle
 
