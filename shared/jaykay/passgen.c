bf3f69c8f166187086dd1205b6aefb6f74c78ef6
Jurgen Kluft2 <jurgen@virtuosgames.com>
2024-08-12 17:11:23 +0800

    * passgen update

--- shared/jaykay/passgen.c
+++ shared/jaykay/passgen.c
@@ -5,7 +5,18 @@ static const char* c_letters = "abcdefghijklmnopqrstuvwxyz";
 static const char* c_numbers = "1234567890";
 static const char* c_symbols = "!@#$&+=-";
 
-void GenPassword(uint64_t seed, bool _uppercase, bool _numbers, bool _symbols, char* output_str, int _output_len)
+// Take a number of bits from the seed and return them.
+// Note: Maximum number of bits that can be taken is 8.
+int8_t TakeBits(uint64_t* seed, int8_t n)
+{
+    int8_t result = *seed & ((1 << n) - 1);
+    *seed >>= n;
+    *seed = *seed | (result << 64 - n);
+    return result;
+
+}
+
+void GenPassword(uint64_t seed, bool _uppercase, bool _numbers, bool _symbols, char* _output_str, int _output_len)
 {
     // seed is 64 bits, maximum 12 character word
 
@@ -21,5 +32,40 @@ void GenPassword(uint64_t seed, bool _uppercase, bool _numbers, bool _symbols, c
     // - if the bit is 2, take 3 bits and use it to obtain a symbol
     // - adjust the character set based on the options
     // - repeat until the output length is reached
+    int8_t type;
+    int8_t index;
+    char c;
+    int output_len;
 
+    output_len = 0;
+    while (output_len < _output_len)
+    {
+        type = TakeBits(&seed, 2);
+        switch (type)
+        {
+            case 0:
+                index = TakeBits(&seed, 5);
+                if (_uppercase)
+                {
+                    c = c_letters[index] - 'a' + 'A';
+                }
+                else
+                {
+                    c = c_letters[index];
+                }
+                break;
+            case 1:
+                index = TakeBits(&seed, 4);
+                c = c_numbers[index];
+                break;
+            case 2:
+            case 3:
+                index = TakeBits(&seed, 3);
+                c = c_symbols[index];
+                break;
+        }
+        _output_str[output_len++] = c;
+    }
+
+    _output_str[_output_len] = '\0';
 }
